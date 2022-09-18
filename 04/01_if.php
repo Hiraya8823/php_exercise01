e6e3c84539d8bcce068334a397e1a4e50789da78
Hiraya8823 <hirayama82007@yahoo.co.jp>
2022-09-18 17:23:41 +0900

    制御構文1の課題

---
+++ 04/01_if.php
@@ -0,0 +1,18 @@
+<?php 
+
+echo '$aの値を入力してください: ';
+$a = trim(fgets(STDIN));
+
+echo '$bの値を入力してください: ';
+$b = trim(fgets(STDIN));
+
+if ($a > $b) {
+    echo "$a";
+} elseif ($a < $b){
+    echo "$b";
+} elseif ($a == $b){
+    echo '入力された値は等しいです';
+};
+
+
+
